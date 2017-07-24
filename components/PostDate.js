58209a68f4b6593a248766e9aa977a794758e9d7
Krzysztof Urbas <krzysu@interia.pl>
2017-07-24 23:40:06 +0200

    fix build issue with date

--- components/PostDate.js
+++ components/PostDate.js
@@ -5,12 +5,11 @@ import 'moment/locale/pl';
 
 const PostDate = ({ date, lang }) => {
     const prefix = lang === 'pl' ? 'Opublikowano' : 'Published';
-    const format = lang === 'pl' ? 'D MMMM YYYY' : 'MMMM Do, YYYY';
+    const formatedDate = lang === 'pl' ?
+        moment(date).locale('pl').format('D MMMM YYYY') : moment(date).locale('en').format('MMMM Do, YYYY');
 
     return (
-        <small>
-            {`${prefix} ${moment(date).locale(lang).format(format)}`}
-        </small>
+        <small>{`${prefix} ${formatedDate}`}</small>
     );
 }
 
