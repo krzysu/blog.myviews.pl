82531387eb1a7ae3e3f92fd51509d53c2bcb6d62
Krzysztof Urbas <krzysu@interia.pl>
2018-02-10 22:51:25 +0700

    new post: management

--- utils/i18n.js
+++ utils/i18n.js
@@ -27,7 +27,7 @@ i18n.init({
                 authorItem: {
                     body: `My name is <1>Kris Urbas</1> and I help companies build their internet products.
                     I offer help in many different areas of software and product development, management, leadership
-                    and team building. With 8 years of professional experience I have worked with more than
+                    and team building. With 9 years of professional experience I have worked with more than
                     10 companies and startups of different sizes.
                     <3>Can I help you? Write me what are you struggling with!</3>`,
                 },
@@ -89,7 +89,7 @@ i18n.init({
                     body: `Nazywam się <1>Krzysiek Urbas</1> i pomagam firmom budować aplikacje internetowe.
                     Oferuję pomoc w wielu różnych obszarach rozwoju oprogramowania i produktu,
                     także zarządzania oraz kierowania i budowania zespołu.
-                    Mam ponad 8 lat doświadczenia zawodowego.
+                    Mam ponad 9 lat doświadczenia zawodowego.
                     Pracowałem już z ponad 10 firmami i startup'ami w różnych rozmiarach.
                     <3>W czym mogę Ci pomóc? Napisz do mnie!</3>`,
                 },
